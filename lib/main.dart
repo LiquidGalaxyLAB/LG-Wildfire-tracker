8ee257dc6a736492ffba505917a8e6f23788ce62
Gerard <gerardmonso@gmail.com>
2023-11-28 01:44:33 +0100

    main sync settings and services for lg

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,56 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:wildfiretracker/screens/splash.dart';
+import 'package:get_it/get_it.dart';
+import 'package:wildfiretracker/screens/splash_screen.dart';
+import 'package:wildfiretracker/services/file_service.dart';
+import 'package:wildfiretracker/services/lg_service.dart';
+import 'package:wildfiretracker/services/lg_settings_service.dart';
+import 'package:wildfiretracker/services/local_storage_service.dart';
+import 'package:wildfiretracker/services/settings_service.dart';
+import 'package:wildfiretracker/services/ssh_service.dart';
+import 'package:wildfiretracker/utils/theme.dart';
 
-void main() {
+/// Registers all services into the application.
+void setupServices() {
+  GetIt.I.registerLazySingleton(() => LocalStorageService());
+  GetIt.I.registerLazySingleton(() => SettingsService());
+  GetIt.I.registerLazySingleton(() => SSHService());
+  GetIt.I.registerLazySingleton(() => LGService());
+  GetIt.I.registerLazySingleton(() => FileService());
+  GetIt.I.registerLazySingleton(() => LGSettingsService());
+}
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  setupServices();
+
+  await GetIt.I<LocalStorageService>().loadStorage();
+
+  GetIt.I<SSHService>().init();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  /// Sets the Liquid Galaxy logos into the rig.
+  void setLogos() async {
+    try {
+      await GetIt.I<LGService>().setLogos();
+    } catch (e) {
+      print(e);
+      if (kDebugMode) {
+        print(e);
+      }
+    }
+  }
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    setLogos();
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Wildfire Tracker',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -29,6 +67,12 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
+        canvasColor: Colors.transparent,
+        primarySwatch: MaterialColor(
+          ThemeColors.backgroundColorHex,
+          ThemeColors.backgroundColorMaterial,
+        ),
+        scaffoldBackgroundColor: ThemeColors.backgroundColor,
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
