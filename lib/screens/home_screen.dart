bee62ac139f4297f60a254c62e5f0feaf6610c59
Gerard <gerardmonso@gmail.com>
2024-01-08 19:17:28 +0100

    home screen desing and

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,82 +5,103 @@ import 'package:wildfiretracker/screens/lg_settings_sreen.dart';
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
-  final String title = 'title';
-
   @override
   State<HomePage> createState() => _HomeState();
 }
 
 class _HomeState extends State<HomePage> {
   List<Map<String, String>> services = [
-    {'name': 'NASA','route': 'nasa',  'imageUrl': 'https://img.icons8.com/external-vitaliy-gorbachev-flat-vitaly-gorbachev/2x/external-cleaning-labour-day-vitaliy-gorbachev-flat-vitaly-gorbachev.png'},
-    {'name': 'Plumber','route': 'nasa', 'imageUrl': 'https://img.icons8.com/external-vitaliy-gorbachev-flat-vitaly-gorbachev/2x/external-plumber-labour-day-vitaliy-gorbachev-flat-vitaly-gorbachev.png'},
-    {'name': 'Electrician','route': 'nasa', 'imageUrl': 'https://img.icons8.com/external-wanicon-flat-wanicon/2x/external-multimeter-car-service-wanicon-flat-wanicon.png'},
-    {'name': 'Painter','route': 'nasa', 'imageUrl': 'https://img.icons8.com/external-itim2101-flat-itim2101/2x/external-painter-male-occupation-avatar-itim2101-flat-itim2101.png'}
+    {
+      'name': 'NASA',
+      'route': '/nasa',
+      'imageUrl':
+          'https://img.icons8.com/external-vitaliy-gorbachev-flat-vitaly-gorbachev/2x/external-cleaning-labour-day-vitaliy-gorbachev-flat-vitaly-gorbachev.png'
+    },
+    {
+      'name': 'Test1',
+      'route': '/nasa',
+      'imageUrl':
+          'https://img.icons8.com/external-vitaliy-gorbachev-flat-vitaly-gorbachev/2x/external-plumber-labour-day-vitaliy-gorbachev-flat-vitaly-gorbachev.png'
+    },
+    {
+      'name': 'Test2',
+      'route': '/nasa',
+      'imageUrl':
+          'https://img.icons8.com/external-wanicon-flat-wanicon/2x/external-multimeter-car-service-wanicon-flat-wanicon.png'
+    },
+    {
+      'name': 'Test3',
+      'route': '/nasa',
+      'imageUrl':
+          'https://img.icons8.com/external-itim2101-flat-itim2101/2x/external-painter-male-occupation-avatar-itim2101-flat-itim2101.png'
+    }
   ];
-
-
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.transparent,
-          title: Text('Home', style: TextStyle(color: Colors.black),),
+          title: Text(
+            'Home',
+            style: TextStyle(color: Colors.black),
+          ),
           elevation: 0,
           actions: [
             IconButton(
               onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) => LGSettings()));
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => LGSettings()));
               },
-              icon: Icon(Icons.settings, color: Colors.grey.shade700, size: 30,),
+              icon: Icon(
+                Icons.settings,
+                color: Colors.grey.shade700,
+                size: 30,
+              ),
             )
           ],
         ),
         body: SingleChildScrollView(
-            child: Column(
-                children: [
-                  Padding(
-                    padding: EdgeInsets.only(left: 20.0, right: 10.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text('Categories', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
-                      ],
-                    ),
-                  ),
-                  Container(
-                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                    height: 400,
-                    child: GridView.builder(
-                        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: 2,
-                          childAspectRatio: 1.0,
-                          crossAxisSpacing: 10.0,
-                          mainAxisSpacing: 10.0,
-                        ),
-                        // physics: NeverScrollableScrollPhysics(),
-                        itemCount: services.length,
-
-                        itemBuilder: (BuildContext context, int index) {
-                          return FadeIn(
-                              duration: Duration(milliseconds: 1000),
-                              delay: Duration(seconds: (1.0 + index).round()),
-                              child: serviceContainer(
-                                  services[index]['imageUrl']!,
-                                  services[index]['name']!,
-                                  services[index]['route']!,
-                                  index)
-                          );
-                        }
-                    ),
-                  ),
-                  SizedBox(height: 20,),
-                ]
-            )
-        )
-    );
+            child: Column(children: [
+          const Padding(
+            padding: EdgeInsets.only(left: 20.0, right: 10.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Categories',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            height: 400,
+            child: GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  childAspectRatio: 1.0,
+                  crossAxisSpacing: 10.0,
+                  mainAxisSpacing: 10.0,
+                ),
+                // physics: NeverScrollableScrollPhysics(),
+                itemCount: services.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return FadeIn(
+                      duration: Duration(milliseconds: 1000),
+                      delay: Duration(seconds: (1.0 + index).round()),
+                      child: serviceContainer(
+                          services[index]['imageUrl']!,
+                          services[index]['name']!,
+                          services[index]['route']!,
+                          index));
+                }),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+        ])));
   }
 
   serviceContainer(String image, String name, String route, int index) {
@@ -100,15 +121,18 @@ class _HomeState extends State<HomePage> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               Image.network(image, height: 45),
-              SizedBox(height: 20,),
-              Text(name, style: TextStyle(fontSize: 15),)
-            ]
-        ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                name,
+                style: TextStyle(fontSize: 15),
+              )
+            ]),
       ),
       onTap: () {
         Navigator.pushNamed(context, route);
       },
     );
   }
-
 }
