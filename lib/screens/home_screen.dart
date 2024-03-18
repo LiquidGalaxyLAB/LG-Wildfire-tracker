e521846509afff69c28fe8df77c0dbd7ab51f256
Gerard <gerardmonso@gmail.com>
2024-03-18 14:31:54 +0100

    home desing

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,7 @@
 import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 import 'package:wildfiretracker/screens/lg_settings_sreen.dart';
-import 'package:wildfiretracker/utils/theme.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -15,25 +15,46 @@ class _HomeState extends State<HomePage> {
     {
       'name': 'NASA - Live Fire',
       'route': '/nasa',
-      'asset': 'assets/images/fire.png',
-      'icon': false
-    },
-    {
-      'name': 'Settings',
-      'route': '/settings',
       'asset': '',
-      'icon': Icons.settings
+      'icon': Icons.local_fire_department,
+      'keys': [
+        {'name': 'Fires', 'icon': Icons.fireplace_sharp},
+        {'name': 'Countries', 'icon': Icons.public_outlined},
+        {'name': 'Satellites', 'icon': Icons.satellite_alt},
+        {'name': 'NASA', 'icon': Icons.data_saver_on}
+      ]
     },
     {
       'name': 'GenCat - Historic Wildfire',
       'route': '/gencat',
       'asset': '',
-      'icon': Icons.forest
+      'icon': Icons.forest,
+      'keys': [
+        {'name': 'Fire perimeters', 'icon': Icons.polyline_outlined},
+        {'name': 'Wildfires', 'icon': Icons.forest_outlined},
+        {'name': 'Catalonia', 'icon': Icons.location_city},
+        {'name': 'Historical', 'icon': Icons.history}
+      ]
+    },
+    {
+      'name': 'Settings',
+      'route': '/settings',
+      'asset': '',
+      'icon': Icons.settings,
+      'keys': [
+        {'name': 'LG Settings', 'icon': Icons.settings},
+        {'name': 'LG Comands', 'icon': Icons.terminal},
+        {'name': 'NASA API', 'icon': Icons.key}
+      ]
     }
   ];
 
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+    final screenAspectRatio = MediaQuery.of(context).size.aspectRatio;
+
     return Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.transparent,
@@ -72,13 +93,13 @@ class _HomeState extends State<HomePage> {
           ),
           Container(
             padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-            height: 400,
+            height: screenHeight,
             child: GridView.builder(
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 4,
-                  childAspectRatio: 1.0,
-                  crossAxisSpacing: 10.0,
-                  mainAxisSpacing: 10.0,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: (screenWidth ~/ 300),
+                  //childAspectRatio: screenAspectRatio*1.5,
+                  crossAxisSpacing: 20.0,
+                  mainAxisSpacing: 20.0,
                 ),
                 // physics: NeverScrollableScrollPhysics(),
                 itemCount: services.length,
@@ -91,6 +112,7 @@ class _HomeState extends State<HomePage> {
                           services[index]['name']!,
                           services[index]['route']!,
                           services[index]['icon']!,
+                          services[index]['keys']!,
                           index));
                 }),
           ),
@@ -100,34 +122,128 @@ class _HomeState extends State<HomePage> {
         ])));
   }
 
-  serviceContainer(String asset, String name, String route, dynamic icon, int index) {
+  serviceContainer(String asset, String name, String route, dynamic icon,
+      List<dynamic> keys, int index) {
     return GestureDetector(
       child: Container(
         margin: EdgeInsets.only(right: 0),
         padding: EdgeInsets.all(10.0),
-        decoration: BoxDecoration(
+        decoration: ShapeDecoration(
           color: Colors.grey.shade100,
-          border: Border.all(
-            color: ThemeColors.primaryColor,
-            width: 2.0,
+          shadows: const [
+            BoxShadow(
+              color: Color(0x26000000),
+              blurRadius: 6,
+              offset: Offset(0, 2),
+              spreadRadius: 2,
+            ),
+            BoxShadow(
+              color: Color(0x4C000000),
+              blurRadius: 2,
+              offset: Offset(0, 1),
+              spreadRadius: 0,
+            )
+          ],
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
           ),
-          borderRadius: BorderRadius.circular(20.0),
         ),
         child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
               if (icon is IconData)
-                Icon(icon,
-                size: 65,),
-              if (asset != '')
-                Image.asset(asset, height: 65),
+                Icon(
+                  icon,
+                  size: 65,
+                ),
+              if (asset != '') Image.asset(asset, height: 65),
               const SizedBox(
                 height: 20,
               ),
               Text(
                 name,
                 style: const TextStyle(fontSize: 20),
-              )
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              ResponsiveGridView.builder(
+                //addAutomaticKeepAlives: true,
+                shrinkWrap: true,
+                gridDelegate: const ResponsiveGridDelegate(
+                  maxCrossAxisExtent: 200,
+                  // Maximum extent of tiles in the grid along the cross axis.
+                  minCrossAxisExtent: 100,
+                  // Minimum extent of tiles in the grid along the cross axis.
+                  childAspectRatio: 3.5,
+                  // Aspect ratio for items.
+                  crossAxisSpacing: 10,
+                  // Horizontal spacing between items.
+                  mainAxisSpacing: 10, // Vertical spacing between items.
+                ),
+                itemCount: keys.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                    clipBehavior: Clip.antiAlias,
+                    decoration: ShapeDecoration(
+                      color: Colors.grey.shade100,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8)),
+                      shadows: const [
+                        BoxShadow(
+                          color: Color(0x26000000),
+                          blurRadius: 3,
+                          offset: Offset(0, 1),
+                          spreadRadius: 1,
+                        ),
+                        BoxShadow(
+                          color: Color(0x4C000000),
+                          blurRadius: 2,
+                          offset: Offset(0, 1),
+                          spreadRadius: 0,
+                        )
+                      ],
+                    ),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.only(
+                              top: 6, left: 8, right: 16, bottom: 6),
+                          child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Container(
+                                alignment: Alignment.center,
+                                transformAlignment: Alignment.center,
+                                child: Icon(
+                                  keys[index]['icon'] as IconData,
+                                  color: Colors.black,
+                                ),
+                              ),
+                              const SizedBox(width: 5),
+                              Text(
+                                keys[index]['name'].toString(),
+                                textAlign: TextAlign.center,
+                                style: const TextStyle(
+                                  color: Color(0xFF49454F),
+                                  fontSize: 11,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
             ]),
       ),
       onTap: () {
